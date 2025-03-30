ca7b7d6f9118c1a3012fdbf42003939afeed63b5
Corentin Leruth <corentin.leruth@gmail.com>
2025-03-30 11:21:00 +0100

    exercise 4

--- src/exercises/section-1/004_creating-a-layer.ts
+++ src/exercises/section-1/004_creating-a-layer.ts
@@ -1,5 +1,7 @@
 // The shared service modules also export pre-defined constructors, given that
 // we don't need to know how the services are implemented to use them
+import { NodeHttpClient } from "@effect/platform-node"
+import { Layer } from "effect"
 import { ImmunityTokenManager, makeImmunityTokenManager } from "./shared/services/ImmunityTokenManager.js"
 import { makePunDistributionNetwork, PunDistributionNetwork } from "./shared/services/PunDistributionNetwork.js"
 import { makePunsterClient, PunsterClient } from "./shared/services/PunsterClient.js"
@@ -33,10 +35,26 @@ import { makePunsterClient, PunsterClient } from "./shared/services/PunsterClien
  */
 
 /* Uncomment the below to implement */
-// export const ImmunityTokenManagerLayer =
+
+export const ImmunityTokenManagerLayer = Layer.scoped(
+  ImmunityTokenManager,
+  makeImmunityTokenManager
+)
 
 /* Uncomment the below to implement */
-// export const PunsterClientLayer =
+export const PunsterClientLayer = Layer.effect(
+  PunsterClient,
+  makePunsterClient
+).pipe(Layer.provide(ImmunityTokenManagerLayer))
 
 /* Uncomment the below to implement */
-// export const PunDistributionNetworkLayer =
+
+export const PunDistributionNetworkLayer = Layer.effect(PunDistributionNetwork, makePunDistributionNetwork).pipe(
+  Layer.provide(PunsterClientLayer)
+)
+
+export const MainLayer = Layer.mergeAll(
+  ImmunityTokenManagerLayer,
+  PunsterClientLayer,
+  PunDistributionNetworkLayer
+).pipe(Layer.provide(NodeHttpClient.layerUndici))
